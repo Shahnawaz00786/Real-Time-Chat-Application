ecb2c8854dcd5e18ef0441a05313483d80c34e88
shahnawaz00786 <mdshahnawaz2493@gmail.com>
2025-10-27 12:44:47 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5175",
+    origin:"http://localhost:5173",
     credentials:true
 }))
 app.use(express.json())
