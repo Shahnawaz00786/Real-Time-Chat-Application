79aa4084a59bb8ed406a63c1dd95d274fdaa669b
Shahnawaz00786 <160699579+Shahnawaz00786@users.noreply.github.com>
2025-10-27 13:16:45 +0530

    Update CORS origin for socket server

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ let app = express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5175"
+        origin:"https://real-time-chat-application-yaic.onrender.com"
     }
 })
  const userSocketMap ={}
